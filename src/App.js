6d1612cddc2c119d5d5e29fb2ca85c9d8687e0de
welinton <willienn@hotmail.com>
2017-03-27 21:19:32 -0500

    Se agrega componente Welcome

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Welcome from './Welcome.js'
 
 class App extends Component {
   constructor() {
---
+++ src/Welcome.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Welcome =(props) => <h1>Hola {props.name} </h1>;
+
+
+
+export default Welcome;
